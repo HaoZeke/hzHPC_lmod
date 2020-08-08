06be3747603a4c6b7c5247883444acad2aca7eb7
Rohit Goswami <rgoswami@ieee.org>
2020-08-08 18:47:50 +0000

    files: Init gperf,ncurses, and texinfo

---
+++ dotfiles/common/Modulefiles/gperf/3.1.lua
@@ -0,0 +1,43 @@
+-- Copyright 2020 Rohit Goswami (HaoZeke) <rog32[at]hi.is>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+--
+-- you may not use this file except in compliance with the License.
+--
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+--
+-- distributed under the License is distributed on an "AS IS" BASIS,
+--
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--
+-- See the License for the specific language governing permissions and
+--
+-- limitations under the License.
+
+help([[
+User module to load a newer library built against gcc 9.2.0
+]])
+
+whatis("Name: " .. myModuleName())
+whatis("Version: " .. myModuleVersion())
+whatis("Category: lib")
+whatis("https://www.gnu.org/software/texinfo/")
+
+-- Dependencies
+always_load("autotools/autotools")
+always_load("texinfo/6.7")
+
+-- Variables
+local home    = os.getenv("HOME")
+local version = myModuleVersion()
+local pkgName = myModuleName()
+local sechome   = pathJoin(home,".hpc",pkgName,version)
+
+-- Paths
+prepend_path("MANPATH",pathJoin(sechome,"share"))
+prepend_path("PATH",pathJoin(sechome,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(sechome,"lib"))
