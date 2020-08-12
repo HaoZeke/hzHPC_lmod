189b3bc4674f6ac52325950428918dc6a55be810
Rohit Goswami <rgoswami@ieee.org>
2020-08-12 13:02:50 +0000

    julia: Init at 1.5.0

---
+++ dotfiles/common/Modulefiles/julia/1.5.0.lua
@@ -0,0 +1,40 @@
+-- Copyright 2020 Rohit Goswami (HaoZeke) <rog32[at]hi.is>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+--
+-- you may not use this file except in compliance with the License.
+--
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+--
+-- distributed under the License is distributed on an "AS IS" BASIS,
+--
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--
+-- See the License for the specific language governing permissions and
+--
+-- limitations under the License.
+
+help([[
+User module to load julia
+]])
+
+whatis("Name: " .. myModuleName())
+whatis("Version: " .. myModuleVersion())
+whatis("Category: lang")
+whatis("https://julialang.org/")
+
+-- Variables
+local home    = os.getenv("HOME")
+local version = myModuleVersion()
+local pkgName = myModuleName()
+local juliah   = pathJoin(home,".hpc",pkgName,version)
+
+-- Paths
+prepend_path("MANPATH",pathJoin(juliah,"share"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(juliah,"lib"))
+prepend_path("INCLUDE",pathJoin(juliah,"include"))
+prepend_path("PATH",pathJoin(juliah,"bin"))
