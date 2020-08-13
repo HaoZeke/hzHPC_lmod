226c8f42edc3efce0a6ce0eb576081ba4658cc3d
Rohit Goswami <rgoswami@ieee.org>
2020-08-13 14:03:36 +0000

    nix: nodeps

--- dotfiles/common/Modulefiles/nix/nix-boot.lua
+++ dotfiles/common/Modulefiles/nix/nix-boot.lua
@@ -28,12 +28,12 @@ whatis("Category: lib")
 whatis("")
 
 -- Dependencies
-always_load("autotools/autotools")
-always_load("pkg-config/0.29.2")
-always_load("bzip2/1.0.8")
-always_load("libseccomp/2.5.0")
-always_load("editline/1.17.1")
-always_load("boost/boost-1.73.0")
+-- always_load("autotools/autotools")
+-- always_load("pkg-config/0.29.2")
+-- always_load("bzip2/1.0.8")
+-- always_load("libseccomp/2.5.0")
+-- always_load("editline/1.17.1")
+-- always_load("boost/boost-1.73.0")
 
 
 -- Variables
