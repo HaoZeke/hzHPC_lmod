6156ef1f7bab2ede75492ef99030839591fe23b4
Rohit Goswami <rgoswami@ieee.org>
2020-08-16 12:55:40 +0000

    config: Init to build nix

---
+++ dotfiles/nixpkgs/.config/nixpkgs/config.nix
@@ -0,0 +1,29 @@
+{
+  packageOverrides = pkgs:
+    with pkgs; {
+      nix = nix.overrideAttrs (oldAttrs: {
+        doCheck = false;
+        doInstallCheck = false;
+        prePatch = ''
+          substituteInPlace src/libstore/local-store.cc \
+            --replace '(eaName == "security.selinux")' \
+                      '(eaName == "security.selinux" || eaName == "system.nfs4_acl")'
+          substituteInPlace src/libstore/gc.cc \
+            --replace 'auto mapLines =' \
+                      'continue; auto mapLines ='
+          substituteInPlace src/libstore/sqlite.cc \
+            --replace 'SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, 0) != SQLITE_OK)' \
+                      'SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, "unix-dotfile") != SQLITE_OK)'
+        '';
+      });
+    };
+}
+
+# pkgs: {
+#   packageOverrides = self: {
+#     nix = self.nix.override {
+#       storeDir = "/users/home/rog32/.nix/store";
+#       stateDir = "/users/home/rog32/.nix/var";
+#     };
+#   };
+# }
